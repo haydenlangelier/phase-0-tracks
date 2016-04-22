9c92987e7810b88568c9f89eca9359a044ac8ce8
Hayden Langelier <haydenlangelier@utexas.edu>
2016-04-22 16:32:42 -0700

    2.0

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -4,9 +4,19 @@ h[:name]=gets.chomp
 puts "what is your age?"
 h[:age]=gets.to_i
 puts "how many kids do you have?"
-h[:numberofchlidren]=gets.to_i
+h[:numberofchildren]=gets.to_i
 puts "what is your favorite decor theme?"
 h[:decortheme]=gets.chomp
 puts "what is your favorite color?"
 h[:favoritecolor]=gets.chomp
 p h
+puts"does this information look correct? Please enter yes or no."
+answer=gets.chomp
+if answer==="no"
+	puts "which do you want to change? age, numberofchildren, decortheme, or favoritecolor"
+	correctkey=gets.chomp.to_s
+	puts "what do you want to change it to?"
+	correctvalue=gets.chomp
+else
+	break
+end
