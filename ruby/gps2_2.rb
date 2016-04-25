f2d3bf58cea7d32ba483837219b6543c9793871e
Hayden Langelier <haydenlangelier@utexas.edu>
2016-04-25 16:46:33 -0700

    2.0

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,28 +1,21 @@
 #GPS 2.2 paired with Hayden Langelier
 
-
-
 # Method to create a list
 #define grocery_list
 #puts "What items would you like to buy today? Please separate with commas."
 #gets string from user   input =gets.chomp
 #split strings to change to array 
-# list=input.split(' ')
-# [[list.each],[1].to_h
-
-  # input =  "carrots apples cereal pizza"
-  # list = input.split(' ')
-  # p list
-  # ['carrots', 'apples', 'cereal', 'pizza']
 
 class Grocery
 
   def new_list
-    puts "please enter the items you want to buy!"
-  input=gets.chomp.split(' ')
+    puts "Please enter the items you want to buy!"
+   input=gets.chomp.split(' ')
    list = {}
+   
    input.each do |item|
-   list[item]=1 
+     puts "What quantity of #{item} do you want?"
+   list[item]=gets.chomp
    p list
    end
     @new_list = list
@@ -30,7 +23,7 @@ class Grocery
   end
 
   def add
-    puts "what else do you want to add?" 
+    puts "What else do you want to add?" 
     newitem=gets.chomp
     puts "how many do you want?"
     newnumber=gets.chomp.to_i
@@ -41,7 +34,7 @@ class Grocery
   end
   
   def remove
-    puts "what item do you want to remove"
+    puts "What item do you want to remove?"
     removeditem=gets.chomp
     list= @new_list
     list.delete(removeditem)
@@ -50,7 +43,7 @@ class Grocery
   end
 
   def update
-    puts "what item do you want to update the quantity?"
+    puts "What item do you want to update the quantity?"
     updateditem=gets.chomp
     puts "how many do you want?"
     updatednumber=gets.chomp
@@ -61,12 +54,12 @@ class Grocery
   end
 
   def entire_list
-    puts "Hello, my name is #{@new_list} and I am #{@add} years old."
+    puts "Here is my fully updated and final grocery list: #{@new_list}."
   end
 
 end
 
 a=Grocery.new
 
-a.new_list.add.remove.update
+a.new_list.add.remove.update.entire_list
   
