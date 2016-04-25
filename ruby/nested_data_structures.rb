91ab22f89ea7b72726b2e1c8958c6b2f8ae4fff4
Hayden Langelier <haydenlangelier@utexas.edu>
2016-04-24 17:00:38 -0700

    1.0

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,42 @@
+
+
+travel = {
+    Americas: {
+        north_america: {
+         USA: ["Los Angeles","New York", "San Francisco", "Seattle", "Miami", "Las Vegas"],
+         Canada: ["Vancouver","Quevec", "Toronto"],
+         Mexico: ["Mexico City","Cancun","Cozumel"]
+        }, 
+        south_america: {
+         Brazil: ["Sao Paulo","Rio de Janeiro",  "Salvador"],
+         Chile: ["Santiago","Puente Alto"],
+         Columbia: ["Bogota","Medellin","Cali"]
+        } 
+    },
+    Europe: {
+        north_europe: {
+         England: ["Los Angeles","New York", "San Francisco", "Seattle", "Miami", "Las Vegas"],
+         Sweden: ["Vancouver","Quevec", "Toronto"],
+         France: ["Mexico City","Cancun","Cozumel"]
+        }, 
+        south_europe: {
+         Italy: ["Sao Paulo","Rio de Janeiro",  "Salvador"],
+         Rome: ["Santiago","Puente Alto"],
+         Greece: ["Bogota","Medellin","Cali"]
+        } 
+    },Asia: {
+        north_asia: {
+         USA: ["Los Angeles","New York", "San Francisco", "Seattle", "Miami", "Las Vegas"],
+         Canada: ["Vancouver","Quevec", "Toronto"],
+         Mexico: ["Mexico City","Cancun","Cozumel"]
+        }, 
+        southeast_asia: {
+         Brazil: ["Sao Paulo","Rio de Janeiro",  "Salvador"],
+         Chile: ["Santiago","Puente Alto"],
+         Columbia: ["Bogota","Medellin","Cali"]
+        } 
+    }
+    
+}
+
+
