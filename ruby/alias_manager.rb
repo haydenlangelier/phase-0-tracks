a5706f51c6205a52147c46d0cfc09228422ecb1f
Hayden Langelier <haydenlangelier@utexas.edu>
2016-04-24 21:34:46 -0700

    updated

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,28 +1,16 @@
 answer =""
 box={}
 until answer === "no"
-
 puts "what is your first name?"
 first=gets.chomp
 puts "what is your last name?"
 last=gets.chomp
-
-
 originalname = first+" "+last
-
 newfirst= first.downcase.tr('aeioubcdfghjklmnpqrstvwxyz','eiouacdfghjklmnpqrstvwxyzb')
-
 newlast= last.downcase.tr('aeioubcdfghjklmnpqrstvwxyz','eiouacdfghjklmnpqrstvwxyzb')
-
 newname = newlast+" "+newfirst
-
 box[originalname]= newname    
-
-puts "Your new name is #{newname}!"
-
-puts "do you have any more names to enter? Please answer yes or no"
+puts "Your new name is #{newname}! Want to enter more names? yes or no?"
 answer=gets.chomp
-
 end 
-
 box.each {|key,value| puts " #{key}'s secret name is #{value}!"}
