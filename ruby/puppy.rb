aa54eaa5afecaf4ee5ba5702686b98fa5ca0c414
Hayden Langelier <haydenlangelier@utexas.edu>
2016-04-25 18:17:49 -0700

    final

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#SPECIES ------------------------
+#Cute canine
+
+#CHARACTERISTICS ----------------
+#Eyelash length: varies
+#Friendly: yes
+#Eye count: 2
+#Name: varies
+#Color: varies
+
+#BEHAVIOR -----------------------
+#Jump
+#Bite
+#Run
