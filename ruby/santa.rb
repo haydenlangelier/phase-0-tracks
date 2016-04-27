1caf2837e644c4accd5c567f54f06f192013dc95
Hayden Langelier <haydenlangelier@utexas.edu>
2016-04-26 19:22:25 -0700

    2.0

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,8 +2,9 @@ class Santa
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!" 
 	end
-	def eat_milk_and_cookies (cookie_type)
+	def eat_milk_and_cookies (cookie)
 		"That was a good #{cookie}"
+		@cookie=cookie
 	end
 	def initialize(gender,ethnicity)
 		puts "initializing Santa instance"
@@ -20,4 +21,13 @@ genders=["male", "female", "bi", "im not sure", "Santa"]
 ehtnicities=["chinese", "french", "cuban", "indian", "elf"]
 cookies=["peanut butter", "sugar", "oatmeal raisin", "macadania nut","chocolate chip"]
 
-cookie.length
+cookies.length.times do |i|
+	puts "Creating a new santa"
+	santas<<Santa.new(genders[i], ehtnicities[i])
+	end
+	p santas
+
+	puts "seeing what cookie they like"
+	santas.each do
+		|x|   x.eat_milk_and_cookies(cookies[x])
+	end
