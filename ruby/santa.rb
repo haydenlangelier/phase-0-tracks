325f856fcd3859862b89e2b5c9cbc644123c5c53
Hayden Langelier <haydenlangelier@utexas.edu>
2016-04-26 18:41:26 -0700

    1.0

---
+++ ruby/santa.rb
@@ -0,0 +1,23 @@
+class Santa
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!" 
+	end
+	def eat_milk_and_cookies (cookie_type)
+		"That was a good #{cookie}"
+	end
+	def initialize(gender,ethnicity)
+		puts "initializing Santa instance"
+		@gender=gender
+		@ethnicity=ethnicity
+	end 
+	age=0
+	reindeer_ranking =["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+end
+
+
+santas = []
+genders=["male", "female", "bi", "im not sure", "Santa"]
+ehtnicities=["chinese", "french", "cuban", "indian", "elf"]
+cookies=["peanut butter", "sugar", "oatmeal raisin", "macadania nut","chocolate chip"]
+
+cookie.length
