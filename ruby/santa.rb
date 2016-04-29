9c888143c4b7b3ecdb307ac772ab466c2309e2e6
Hayden Langelier <haydenlangelier@utexas.edu>
2016-04-28 19:25:00 -0700

    1.0

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,16 +3,39 @@ class Santa
 		puts "Ho, ho, ho! Haaaappy holidays!" 
 	end
 	def eat_milk_and_cookies (cookie)
-		"That was a good #{cookie}"
+		puts "That was a good #{cookie}"
 		@cookie=cookie
 	end
 	def initialize(gender,ethnicity)
 		puts "initializing Santa instance"
 		@gender=gender
 		@ethnicity=ethnicity
-	end 
-	age=0
+		age=0
 	reindeer_ranking =["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	@reindeer_ranking=reindeer_ranking
+	end 
+	
+	def newgender=(newgenders)
+		@gender=newgenders
+	end
+	def celebratebirthday=(age)
+		@age=age
+		puts "happy #{age}st birthday!"
+	end
+	def get_mad_at=(name)
+         @name=name
+		@reindeer_ranking.delete(@name)
+		@reindeer_ranking.push(@name)
+		p @reindeer_ranking
+	end
+
+	def age
+		@age
+	end
+	def ethnicity
+		@ethnicity
+	end
+
 end
 
 
@@ -27,7 +50,13 @@ cookies.length.times do |i|
 	end
 	p santas
 
-	puts "seeing what cookie they like"
-	santas.each do
-		|x|   x.eat_milk_and_cookies(cookies[x])
-	end
+	
+santas[0].newgender=("female")
+
+ santas[0].celebratebirthday=(1)
+
+ santas[0].get_mad_at=("Dasher") 
+
+ santas[0].age
+
+ santas[0].ethnicity
