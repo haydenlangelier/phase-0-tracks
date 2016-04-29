9497e6a044215a47da2d9502ffac4751bfe733f1
Hayden Langelier <haydenlangelier@utexas.edu>
2016-04-28 20:49:46 -0700

    final

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,23 +7,26 @@ class Santa
 	def eat_milk_and_cookies (cookie)
 		puts "That was a good #{cookie}"
 		@cookie=cookie
+	
 	end
-	def initialize(gender,ethnicity)
+	def initialize(gender,ethnicity, age)
 		puts "initializing Santa instance"
 		@gender=gender
 		@ethnicity=ethnicity
-		age=0
+		
 		@age=age
-	reindeer_ranking =["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	@reindeer_ranking=reindeer_ranking
+		age=0
+	
 	end 
 	
 
-	def celebrate_birthday
-		@age=age
-		puts "happy #{age}st birthday!"
+	def celebratebirthday
+		
+		puts "happy #{@age}st birthday!"
 	end
 	def get_mad_at=(name)
+			reindeer_ranking =["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	@reindeer_ranking=reindeer_ranking
          @name=name
 		@reindeer_ranking.delete(@name)
 		@reindeer_ranking.push(@name)
@@ -37,23 +40,20 @@ genders=["male", "female", "bi", "im not sure", "Santa"]
 ehtnicities=["chinese", "french", "cuban", "indian", "elf"]
 cookies=["peanut butter", "sugar", "oatmeal raisin", "macadania nut","chocolate chip"]
 
-cookies.length.times do |i|
+
+ 100.times do |i|
 	puts "Creating a new santa"
-	santas<<Santa.new(genders[i], ehtnicities[i])
+	santas<<Santa.new(genders.sample, ehtnicities.sample, rand(140))
 	end
-	p santas
-
 	
-santas[0].gender=("female")
-
-
+	p santas
+	
+	santas[0].gender=("female")
 
  santas[0].get_mad_at=("Dasher") 
 
- santas[0].age =3
+ santas[0].age=10
  
-  santas[0].ethnicity
-  
-   santas[0].age =3
-   
-   santas[0].celebrate_birthday
+ santas[0].celebratebirthday
+
+ santas[0].ethnicity
