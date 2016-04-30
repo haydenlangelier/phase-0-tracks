025ec00ac12c28eb45f043010a1d6e338e53f201
Hayden Langelier <haydenlangelier@utexas.edu>
2016-04-30 12:50:06 -0700

    final

--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,3 +1,25 @@
+class TodoList
+ attr_accessor :a
+
+ def initialize (array)
+   @a = array
+ end
+ def get_items
+   p @a
+ end
+ def add_item(new_item)
+   @a.push(new_item)
+ end
+ def delete_item(deletethis)
+   @a.delete(deletethis)
+ end
+ def get_item(index)
+   @a[index]
+ end
+end
+
+
+
 describe TodoList do
   let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
 
