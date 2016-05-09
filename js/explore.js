74938ac7772c63fd7276836c67393ff35107c647
Hayden Langelier <haydenlangelier@utexas.edu>
2016-05-08 19:49:55 -0700

    1.0

---
+++ js/explore.js
@@ -0,0 +1,21 @@
+// var reverse (string){
+//	
+//method that returns string in js
+//return reversed string
+//}
+//reverse ("aubrie")
+//reverse ("ekans")
+
+
+function reverse (string){
+
+	var splitup=string.split("");
+	var backwards=splitup.reverse();
+	var result=backwards.join("");
+	console.log (result);
+}
+
+
+reverse("aubrie")
+reverse("ekans")
+
