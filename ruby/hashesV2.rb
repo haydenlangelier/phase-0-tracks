36b356797038d8dcbee25568bc240e8198b3a788
Hayden Langelier <haydenlangelier@utexas.edu>
2016-05-15 14:33:31 -0700

    setting up hash

---
+++ ruby/hashesV2.rb
@@ -0,0 +1,8 @@
+puts "What is your client's name?"
+name =gets.chomp
+puts "How many children do they have?"
+numchild=gets.to_i
+puts "Favorite decor theme?"
+favdecor=gets.chomp
+puts "Do they like blue? Please type yes or no."
+likesblue=gets.downcase.chomp
