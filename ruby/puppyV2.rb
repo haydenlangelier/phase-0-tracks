07a68c47021e784b72bffbdac45ab73ae09c744a
Hayden Langelier <haydenlangelier@utexas.edu>
2016-05-18 15:06:36 -0700

    done with the puppy

---
+++ ruby/puppyV2.rb
@@ -0,0 +1,14 @@
+#SPECIES ------------------------
+#tiny dog
+
+#CHARACTERISTICS ----------------
+#Eyelash length: 10cm
+#Friendly: yes
+#Eye count: 2
+#Name: Scruffy
+#Color: Brown
+
+#BEHAVIOR -----------------------
+#Jump
+#Fetch
+#Play dead
