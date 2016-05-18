f65fbf1b9831c104a46ecaa96044c66c915d7f3e
Hayden Langelier <haydenlangelier@utexas.edu>
2016-05-18 15:58:53 -0700

    created an array with a loop for the Santas

--- ruby/santav2.rb
+++ ruby/santav2.rb
@@ -14,11 +14,20 @@ class Santa
 	def method_name
 		
 	end
-	def method_name
-		
-	end
+	@age=0
+	@reindeer_ranking=["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 end	
 
-james=Santa.new
+james=Santa.new("male","French")
 james.speak
 james.eat_milk_and_cookies("peanut butter")
+
+
+santas=[]
+genders=["male","female","unkown","elf","Santa"]
+ethnicities=["Korean","Afghan","Thai","Hong Kong-nese", "Canadian"]
+
+genders.length.times do |x|
+	santas << Santa.new(genders[x],ethnicities[x])
+	puts "There are now #{santas.length} Santas instances in the array!"
+end
