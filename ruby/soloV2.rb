74fd1913af820b9a305396f4f9cf24c72f28d402
Hayden Langelier <haydenlangelier@utexas.edu>
2016-05-22 14:04:01 -0700

    pseudocoded my class

---
+++ ruby/soloV2.rb
@@ -0,0 +1,19 @@
+# class Teacher
+#attr reader/accessor
+#def intialize(age,grade, specialty)
+#@age=age
+# @grade=grade
+# @specialty=specialty
+#def greet class
+#puts "hello #{@grade} grade class, how are you?"
+#
+#
+#def yell(students_name)
+#
+#puts "#{students_name}, what are you doing? Stop this instant!"
+#
+#
+#def grade_exam
+#
+#puts "**grades #{@specialty} exams**"
+#
