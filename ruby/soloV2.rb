5b8ee35bd49bd72be2add62bd5e68b9d5c65063a
Hayden Langelier <haydenlangelier@utexas.edu>
2016-05-22 14:13:20 -0700

    set up class and tested with driver code

--- ruby/soloV2.rb
+++ ruby/soloV2.rb
@@ -17,3 +17,37 @@
 #
 #puts "**grades #{@specialty} exams**"
 #
+
+
+class Teacher
+	attr_reader :age, :specialty
+	attr_accessor :grade
+	def initialize(age,grade,specialty)
+		@age=age
+		@grade=grade
+		@specialty=specialty
+	end
+
+	def greet_class
+		puts "Hello #{@grade} grade class, how are you today?"
+	end
+
+	def yell(student)
+		puts "#{student}, what are you doing? Stop this instant!"
+
+	end
+
+	def grade_exam
+		puts "Grades #{@specialty} exams!"
+	end
+
+end
+
+
+alex=Teacher.new(32,"4th","History")
+p alex.age
+alex.grade="11th"
+p alex.grade
+alex.greet_class
+alex.yell("Alice")
+alex.grade_exam
