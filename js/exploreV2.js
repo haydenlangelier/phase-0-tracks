f38b4ec643f9fc57af336b873041856e39121a5a
Hayden Langelier <haydenlangelier@utexas.edu>
2016-05-25 14:11:46 -0700

    almost done tweaking the final function

--- js/exploreV2.js
+++ js/exploreV2.js
@@ -1,12 +1,30 @@
+// function reverse(string)
+//
+//string.split
+//string.reverse
+//string.join
+//console.log
+//
+//
 function reverse (string){
 
   var splitted=string.split("");
   var reversed=splitted.reverse();
   var result=reversed.join("");
-  console.log (result);
+  console.log(result);
 }
 
 
-reverse("jacob")
-reverse("hannah")
-reverse("cheese")
+var a=reverse("jacob")
+var b=reverse("hannah")
+var b=reverse("cheese")
+
+function printif (input){
+if  (input.length>0){
+  console.log(input)
+}
+else  {
+  console.log("error!")}}
+
+
+printif
