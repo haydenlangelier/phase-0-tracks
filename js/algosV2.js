9f9f5c848fca5424dc56de6cb52bbf49055b6bd8
Hayden Langelier <haydenlangelier@utexas.edu>
2016-05-27 21:44:24 -0700

    added kev value match function andt tested with driver code

--- js/algosV2.js
+++ js/algosV2.js
@@ -1,5 +1,42 @@
-function longest_phrase(array){
+//
+//function sortlongest(array)
+//
+//
+//must find length for each
+//.length method
+//sort method from longest to shortest
+//return b.length - a.length
+//function(a,b)
+//call .sort on the inputted array
+//pass in function function(a,b){
+//	return b.length-a.length
+// }
 
-
-	
+function sortlongest (array){
+	var ranked= array.sort(function(a,b){return b.length -a.length});
+	console.log(ranked[0]);
 }
+
+listofstuff=["kats", "3", "christmas", "massachusetts"]
+
+sortlongest(listofstuff)
+
+//function compare (object1, object2){}
+// if  (object1.age==object2.age || object1.name==object2.name){
+//return true};
+//else {
+//return false
+//};
+
+var compare=function (person1, person2){
+	if (person1.age == person2.age|| person1.name == person2.name){
+		return true;}
+	else  {
+		return false;
+	}
+}
+
+thisdude={age:64,name:"hayden"};
+thisgirl={ age:64,name:"cynthia"};
+
+compare(thisgirl,thisdude)
